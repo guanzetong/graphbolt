767fe50fc0cbe258ef008efc76d9b3f59c595b17
Sanjay Sri Vallabh Singapuram <singam@caliber.eecs.umich.edu>
2022-12-01 15:08:01 -0500

    Triangle Counting Algo

--- apps/triangle_counting_MVP.C
+++ apps/triangle_counting_MVP.C
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+unsigned long search_triangles(graph<symmetricVertex> G);
+
 int main (int argc, char *argv[]) {
     // Initialization
     commandLine P(argc, argv);
@@ -23,5 +25,34 @@ int main (int argc, char *argv[]) {
 
 }
 
+unsigned long search_triangles(graph<symmetricVertex> G) {
+    unsigned long count = 0;
+
+    for (int node1 = 0; node1 < G.n; ++node1) {
+        const int node1_degree = G.V[node1].getInDegree();
+        for (int node1_neighbour = 0; node1_neighbour < node1_degree; ++node1_neighbour) {
+            const int node2 = G.V[node1].getInNeighbor(node1_neighbour);
+            if (node2 > node1) {
+                continue;
+            }
+            const int node2_degree = G.V[node2].getInDegree();
+            for (int node2_neighbour = 0; node2_neighbour < node2_degree; ++node2_neighbour) {
+                const int node3 = G.V[node2].getInNeighbor(node2_neighbour);
+                if (node3 > node2) {
+                    continue;
+                }
+                const int node3_degree = G.V[node3].getInDegree();
+                for (int node3_neighbour = 0; node3_neighbour < node3_degree; ++node3_neighbour) {
+                    if(G.V[node3].getInNeighbor(node3_neighbour) == node1) {
+                        ++count;
+                    }
+                }
+            }
+        }
+        
+    }
+
+    return count;
+}
 
 #endif
